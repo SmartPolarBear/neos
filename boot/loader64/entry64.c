c50bebc1ecb985f0d8f9084ac3935a1ac4bf2a10
SmartPolarBear <clevercoolbear@outlook.com>
2023-09-07 13:05:07 +0800

    add skeleton for neldr

--- boot/loader64/entry64.c
+++ boot/loader64/entry64.c
@@ -3,6 +3,12 @@
 //
 #include "ext.h"
 
+
+// NELDR do following things:
+// 1) first, load kernel binaries, as well as smp initialization code
+// 2) place physical memory pages just after kernel binaries
+// 3) scan hardware and place information after pages
+// 4) jmp to kernel entry
 void NO_RETURN LoaderMain64()
 {
 	for (;;)
