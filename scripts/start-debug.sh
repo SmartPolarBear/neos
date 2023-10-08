4fedebda69f3591fb29ed3c9096b701d91cdae1f
SmartPolarBear <clevercoolbear@outlook.com>
2023-10-08 13:21:09 +0800

    feat(ext2loader): it in theorem work.

---
+++ scripts/start-debug.sh
@@ -0,0 +1,23 @@
+qemu-system-x86_64 \
+    -vga std \
+    -d int,cpu_reset \
+    -no-shutdown \
+    -no-reboot \
+    -m 8G \
+    -smp 4 \
+    -machine pc \
+    -s -S \
+    -drive file=scripts/disk.img,if=ide \
+
+
+# qemu-system-x86_64 \
+#     -vga std \
+#     -d int,cpu_reset \
+#     -no-shutdown \
+#     -no-reboot \
+#     -m 8G \
+#     -smp 4 \
+#     -machine pc \
+#     -drive file=scripts/disk.img,index=0,media=disk,format=raw,id=disk0,if=none \
+#                         -device ahci,id=ahci \
+#                         -device ide-hd,drive=disk0,bus=ahci.0 \
